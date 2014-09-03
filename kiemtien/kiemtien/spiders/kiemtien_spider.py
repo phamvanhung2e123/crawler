b647c1289e7420aa3f56d2d60fa82f880fd4f416
phamvanhung <phamvanhung2e123@gmail.com>
2014-09-03 21:15:17 +0900

    fix

---
+++ kiemtien/kiemtien/spiders/kiemtien_spider.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+from scrapy.spider import Spider
+import os
+import urllib
+import re
+from scrapy.selector import Selector
+
+class KiemtienSpider(Spider):
+    name = "kiemtien"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "file:///Users/phamvanhung/Downloads/chokai/chokai1.2/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai1.3/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai1.4/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai1.5/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai2.1/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai2.2/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai2.3/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai2.4/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai2.6/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai2.7/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai3.1/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai3.2/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai3.3/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai3.4/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai3.5/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai4.1/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai4.2/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai4.3/N2 Choukai Soumatome.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai4.4/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai4.5/index.php.html",
+        "file:///Users/phamvanhung/Downloads/chokai/chokai5.1_end/index.php.html",
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2]
+        sel = Selector(response)
+        audios = sel.xpath('//audio/source/@src').extract()
+        for audio in audios:
+            print audio
+            audio_url = 'http://old.lophoctiengnhat.com/' + audio
+            audio_name = re.split('/',audio)[2]
+            print audio_url
+            print audio_name
+            self.download_audio(filename,audio_name,audio_url)
+
+
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+
+    def download_audio(self, folder_name, audio_name, audio_url):
+        try:
+            image_on_web = urllib.urlopen(audio_url)
+            if image_on_web.headers.maintype == 'mp3':
+                print audio_url
+                print audio_name
+                buf = image_on_web.read()
+                path = '/Users/phamvanhung/Downloads/choukai/' + folder_name + '/Audio/FD6/'
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                file_path = "%s%s" % (path, audio_name)
+                downloaded_image = file(file_path, "wb")
+                downloaded_image.write(buf)
+                downloaded_image.close()
+                image_on_web.close()
+
+            else:
+                return False
+        except:
+            return False
+        return True
+
+
---
+++ tutorial1/tutorial1/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for tutorial1 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'tutorial1'
+
+SPIDER_MODULES = ['tutorial1.spiders']
+NEWSPIDER_MODULE = 'tutorial1.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'tutorial1 (+http://www.yourdomain.com)'
